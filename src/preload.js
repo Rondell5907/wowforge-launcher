09d833aa4f3c6cee32570179688c7065b361f696
Rondell5907 <mountaincommunityjellico@gmail.com>
2026-05-15 23:51:15 -0400

    updated contextBridge.

--- src/preload.js
+++ src/preload.js
@@ -1,15 +1,16 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
 contextBridge.exposeInMainWorld('api', {
-  minimize:      ()              => ipcRenderer.invoke('app:minimize'),
-  close:         ()              => ipcRenderer.invoke('app:close'),
-  readConfig:    ()              => ipcRenderer.invoke('config:read'),
-  openConfigDir: ()              => ipcRenderer.invoke('config:openDir'),
-  readSession:   ()              => ipcRenderer.invoke('session:read'),
-  clearSession:  ()              => ipcRenderer.invoke('session:clear'),
-  dbConnect:     ()              => ipcRenderer.invoke('db:connect'),
-  login:         (u, p, rem)    => ipcRenderer.invoke('auth:login', { username: u, password: p, rememberMe: rem }),
-  fetchChars:    (accountId)    => ipcRenderer.invoke('chars:fetch', { accountId }),
-  fetchArticles: ()              => ipcRenderer.invoke('articles:fetch'),
-  launchWow:     ()              => ipcRenderer.invoke('wow:launch'),
+  minimize:      ()           => ipcRenderer.invoke('app:minimize'),
+  close:         ()           => ipcRenderer.invoke('app:close'),
+  readConfig:    ()           => ipcRenderer.invoke('config:read'),
+  writeConfig:   (u)          => ipcRenderer.invoke('config:write', u),
+  openConfigDir: ()           => ipcRenderer.invoke('config:openDir'),
+  browseExe:     ()           => ipcRenderer.invoke('dialog:browseExe'),
+  readSession:   ()           => ipcRenderer.invoke('session:read'),
+  clearSession:  ()           => ipcRenderer.invoke('session:clear'),
+  dbConnect:     ()           => ipcRenderer.invoke('db:connect'),
+  login:         (u,p,rem)    => ipcRenderer.invoke('auth:login', { username:u, password:p, rememberMe:rem }),
+  fetchChars:    (id)         => ipcRenderer.invoke('chars:fetch', { accountId:id }),
+  fetchArticles: ()           => ipcRenderer.invoke('articles:fetch'),
+  launchWow:     ()           => ipcRenderer.invoke('wow:launch'),
 });
